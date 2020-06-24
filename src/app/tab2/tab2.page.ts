04c2fe7bebd549c13bb230bcb4e226bb6b3ae380
elisasanches <60738579+elisasanches@users.noreply.github.com>
2020-06-24 19:01:57 -0300

    Desenvolvimento de Calculadoras
    Desenvolvendo e aprendendo fazer calculadoras

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,25 +8,48 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   public calculo = ''; // vazia
-  public resultado: string;
+  public resultado: string; // null
+
+  private ponto = false;
+  private operacoes = ['+', '-', '*', '/'];
 
   constructor() {}
 
-public adicionarNumero(valor) {
+public adicionarNumero(valor: string) {
   this.calculo = this.calculo + valor;
 }
 
 public adicionarPonto() {
+  if (this.ponto) {
+    return; 
+  } 
   this.calculo += ".";
+  this.ponto = true;
 }
 
 public adicionarOperacao(operador: string) {
+const ultimo = this.calculo.slice(-1);
+
+if (this.operacoes.indexOf(ultimo) > -1) {
+  return;
+}
+
    this.calculo += operador;
+   this.ponto = false;
 }
 
 public apagarTudo() {
    this.calculo = '';
    this.resultado = null;
+   this.ponto = false;
+}
+
+public apagarUltimo() {
+  const ultimo = this.calculo.slice(-1);
+  if (ultimo == '.') {
+    this.ponto = false;
+  }
+  this.calculo = this.calculo.slice(0, -1);
 }
 
 }
